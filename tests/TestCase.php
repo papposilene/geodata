e15c93b8b2a8c3b60d16020c0e1725c1ce7d70e6
Philippe-Alexandre P <papposilene@users.noreply.github.com>
2022-03-07 17:29:09 +0100

    Add files via upload

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Papposilene\Geodata\Test;
+
+use PHPUnit\Framework\TestCase;
+use Papposilene\Geodata\Contracts\Continent;
+use Papposilene\Geodata\Contracts\Subcontinent;
+use Papposilene\Geodata\GeodataRegistrar;
+use Papposilene\Geodata\GeodataServiceProvider;
+
+/**
+ * @category Test
+ * @package  Papposilene\Geodata\Tests
+ * @author   Philippe-Alexandre Pierre <dev@psln.nl>
+ */
+abstract class TestCase 
+{
+
+
+}
