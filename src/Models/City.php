ffc4e06bd9b2ee6590f6250277c814b8d3200405
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2022-03-31 14:50:45 +0200

    Models: update cities

--- src/Models/City.php
+++ src/Models/City.php
@@ -21,6 +21,16 @@ class City extends Model
     protected $primaryKey = 'uuid';
 
     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        //'uuid' => 'uuid',
+        'extra' => 'array',
+    ];
+
+    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
